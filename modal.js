b8dc2a322356b8aed70e6533b2baedbbd07a764a
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-01-27 13:20:01 +0100

    added modal.js

---
+++ modal.js
@@ -0,0 +1,8 @@
+
+const modal = document.querySelector('.backdrop');
+const closeButton = document.querySelector('.close-ellipse');
+
+
+closeButton.addEventListener('click', function() {
+    modal.classList.add('is-hidden');  
+  });
